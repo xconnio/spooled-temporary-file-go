b3bd611dbd8aa06e8b80db7e735a93024c1b2bad
asimfarooq5 <asimfarooq5@gmail.com>
2023-09-19 19:47:43 +0500

    add test for write

--- spooled_test.go
+++ spooled_test.go
@@ -1,10 +1,27 @@
-package spooledtempfile_test
+package spooledtempfile
 
 import (
-	"log"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
-func TestHello(t *testing.T) {
-	log.Println("DONE")
+func TestSpooledTemporaryFile_Write(t *testing.T) {
+	stf := NewSpooledTemporaryFile(30, nil)
+
+	testData := []byte("Hello World!")
+	n, err := stf.Write(testData)
+	assert.NoError(t, err)
+	assert.Equal(t, len(testData), n)
+
+	// Check the sizeWrote field.
+	assert.Equal(t, n, stf.sizeWrote)
+
+	// Attempt to write more than the max size.
+	testData = []byte("This is too much data!")
+	n, err = stf.Write(testData)
+	assert.Equal(t, len(testData), n)
+
+	// Check that it's rolled over after reaching the max size.
+	assert.True(t, stf.rolledOver)
 }
