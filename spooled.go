711ca40b6e7f1f507d981707fbcf899ce4ad6eeb
Asim Farooq <asimfarooq5@gmail.com>
2023-09-20 16:58:15 +0500

    start implementation (#2)
    * start implementation
    
    * if rolledOver, append to file
    
    * append bytes
    
    * add test for write
    
    * add test for read

--- spooled.go
+++ spooled.go
@@ -2,7 +2,7 @@ package spooledtempfile
 
 import (
 	"io"
-	"io/fs"
+	"os"
 )
 
 type SpooledTemporaryFile struct {
@@ -16,7 +16,7 @@ type SpooledTemporaryFile struct {
 	// rolledOver keeps track if the content was dumped to file.
 	rolledOver bool
 	buffer     []byte
-	file       fs.File
+	file       *os.File
 
 	io.Reader
 	io.Writer
@@ -34,24 +34,72 @@ func NewSpooledTemporaryFile(maxSize int, buffer []byte) *SpooledTemporaryFile {
 // may directly create a file if the size of the provided buffer
 // is larger than the max allowed size.
 func (s *SpooledTemporaryFile) Write(bytes []byte) (int, error) {
-	return 0, nil
+	if s.rolledOver {
+		return s.file.Write(bytes)
+	}
+
+	if s.sizeWrote+len(bytes) > s.sizeMax {
+		if err := s.Rollover(); err != nil {
+			return len(bytes), err
+		}
+	}
+
+	s.buffer = append(s.buffer, bytes...)
+	s.sizeWrote += len(bytes)
+
+	return len(bytes), nil
 }
 
 // Read copies the content of the internal buffer OR the file into
 // the provided buffer.
 func (s *SpooledTemporaryFile) Read(bytes []byte) (int, error) {
-	return 0, nil
+	if s.rolledOver {
+		return s.file.Read(bytes)
+	}
+
+	n := copy(bytes, s.buffer)
+	return n, nil
 }
 
 // Close will close the underlying file object. It is NOOP
 // when the buffer is still in-memory.
 func (s *SpooledTemporaryFile) Close() error {
+	if s.rolledOver && s.file != nil {
+		return s.file.Close()
+	}
 	return nil
 }
 
 // Rollover will write the buffer to file even if its
 // smaller than sizeMax.
 func (s *SpooledTemporaryFile) Rollover() error {
+	if s.rolledOver {
+		return nil
+	}
+
+	file, err := os.CreateTemp("", "spooled_temp_file")
+	if err != nil {
+		return err
+	}
+	defer os.Remove(file.Name())
+
+	_, err = file.Write(s.buffer[:s.sizeWrote])
+	if err != nil {
+		return err
+	}
+
+	s.file = file
 	s.rolledOver = true
+
+	s.buffer = nil
+
 	return nil
+}
+
+func (s *SpooledTemporaryFile) SizeWrote() int {
+	return s.sizeWrote
+}
+
+func (s *SpooledTemporaryFile) RolledOver() bool {
+	return s.rolledOver
 }
--- spooled_test.go
+++ spooled_test.go
@@ -1,10 +1,45 @@
 package spooledtempfile_test
 
 import (
-	"log"
+	"github.com/stretchr/testify/require"
+	spooledtempfile "github.com/xconnio/spooled-temporary-file"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
-func TestHello(t *testing.T) {
-	log.Println("DONE")
+func TestSpooledTemporaryFile_Write(t *testing.T) {
+	stf := spooledtempfile.NewSpooledTemporaryFile(30, nil)
+
+	testData := []byte("Hello World!")
+	n, err := stf.Write(testData)
+	assert.NoError(t, err)
+	assert.Equal(t, len(testData), n)
+
+	// Check the sizeWrote field.
+	assert.Equal(t, n, stf.SizeWrote())
+
+	// Attempt to write more than the max size.
+	testData = []byte("This is too much data!")
+	n, err = stf.Write(testData)
+	assert.Equal(t, len(testData), n)
+
+	// Check that it's rolled over after reaching the max size.
+	assert.True(t, stf.RolledOver())
+}
+
+func TestSpooledTemporaryFile_Read(t *testing.T) {
+	stf := spooledtempfile.NewSpooledTemporaryFile(1024, nil)
+
+	data := []byte("Hello World!")
+
+	_, err := stf.Write(data)
+	require.NoError(t, err)
+
+	readBuffer := make([]byte, len(data))
+
+	n, err := stf.Read(readBuffer)
+	require.NoError(t, err)
+	require.Equal(t, len(data), n)
+	require.Equal(t, data, readBuffer)
 }
